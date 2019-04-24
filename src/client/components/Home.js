5301ba5c45ed2415b51b498441e3f83d85546e49
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-04-24 09:02:39 -0500

    converted common js modules to es2015 modules

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      <div>Home Component</div>
+      <button onClick={() => console.log('PUSHED')}>Push Me</button>
+    </div>
+  )
 }
 
 export default Home
