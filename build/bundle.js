93151f6c08a673e7ecbf7603773cdc111f53192a
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-04-24 09:20:41 -0500

    setup webpack for the client

--- build/bundle.js
+++ build/bundle.js
@@ -7047,10 +7047,14 @@ var _Home2 = _interopRequireDefault(_Home);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
+app.use(_express2.default.static('public'));
 
 app.get('/', function (req, res) {
   var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
-  res.send(content);
+
+  var html = '<html>\n      <head>\n        <body>\n          <div id="root">' + content + '</div>\n          <script src="bundle.js"></script>\n        </body>\n      </head>\n    </html>';
+
+  res.send(html);
 });
 
 app.listen(3000, function () {
